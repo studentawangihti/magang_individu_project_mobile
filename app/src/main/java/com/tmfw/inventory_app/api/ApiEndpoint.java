7a1b482a20446dab09e22d6cc4be897363105458
awangihti <studentawangihti@gmail.com>
2025-12-16 09:13:22 +0700

    tambah koneksi aset

--- app/src/main/java/com/tmfw/inventory_app/api/ApiEndpoint.java
+++ app/src/main/java/com/tmfw/inventory_app/api/ApiEndpoint.java
@@ -1,16 +1,25 @@
 package com.tmfw.inventory_app.api;
 
+import com.tmfw.inventory_app.model.AssetResponse;
 import com.tmfw.inventory_app.model.LoginResponse;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiEndpoint {
+
+    // Endpoint Login yang sudah ada (dari Auth.php)
     @FormUrlEncoded
-    @POST("api/auth/login") // Sesuaikan path ini jika berbeda di server
+    @POST("auth/login")
     Call<LoginResponse> login(
             @Field("username") String username,
             @Field("password") String password
     );
+
+    // [BARU] Endpoint untuk mengambil daftar Asset (dari Asset.php)
+    // URL lengkap akan menjadi: BASE_URL + "asset"
+    @GET("asset")
+    Call<AssetResponse> getAssets();
 }
